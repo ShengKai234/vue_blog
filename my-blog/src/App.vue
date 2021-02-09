99bac96251e4ea01f9f83250a725dddfc3108de7
ShengKai234 <bboykai0504@gmail.com>
2021-02-10 00:33:14 +0800

    create simple home

---
+++ my-blog/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <app-menu></app-menu>
+    <router-view></router-view>
+    <app-foolter></app-foolter>
+  </div>
+</template>
+
+<script>
+import Header from './components/include/header'
+import Footer from './components/include/footer'
+import Menu from './components/include/menu'
+
+export default {
+  name: 'App',
+  components: {
+    'app-header': Header,
+    'app-foolter': Footer,
+    'app-menu': Menu
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+.card {
+  vertical-align: middle;
+}
+</style>
---
+++ my-blog/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- <button class="btn btn-danger btn-rounded" v-on:click="login">Sign in!</button> -->
+        <h1>{{ msg }}</h1>
+        <div>
+            <li v-for="item in articles" v-bind:key="item.id">{{ item.title }}</li>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Kai\'s Blog',
+      articles: [
+        {id: 0, title: 'article 1'},
+        {id: 1, title: 'article 2'},
+        {id: 2, title: 'article 3'},
+        {id: 3, title: 'article 4'}
+      ]
+    }
+  },
+  methods: {
+    login: function () {
+      this.$router.push({path: '/Login'})
+    }
+  }
+}
+</script>
